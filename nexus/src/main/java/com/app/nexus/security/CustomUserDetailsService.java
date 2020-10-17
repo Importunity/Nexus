4e6b91d279356dbce0a6cba5c91835f73a12e591
Amadeus Her <Amadeusherr@gmail.com>
2020-10-17 01:13:57 -0500

    Create custom user detail service for loading a user's data base on input

---
+++ nexus/src/main/java/com/app/nexus/security/CustomUserDetailsService.java
@@ -0,0 +1,41 @@
+package com.app.nexus.security;
+
+import com.app.nexus.model.ApplicationUser;
+import com.app.nexus.repository.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import javax.transaction.Transactional;
+
+/**
+ * @Author Amadeus
+ * used to load a users data given the username
+ */
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    ApplicationUserRepository applicationUserRepository;
+
+
+    @Override
+    @Transactional
+    // loads the application user if exists with the email or username entered otherwise throw a usernamenotfoundexception
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        ApplicationUser applicationUser = applicationUserRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("Invalid Username or Email"));
+        return UserPrincipal.create(applicationUser);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        // loads the application user by the Id otherwwise throw usernamenotfound
+        ApplicationUser applicationUser = applicationUserRepository.findById(id).orElseThrow(
+                () -> new UsernameNotFoundException("Application user not found with id : " + id)
+        );
+
+        return UserPrincipal.create(applicationUser);
+    }
+}
