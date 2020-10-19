c8c10a059b90525398c26a0e5c7ae7690fbcb122
Amadeus Her <Amadeusherr@gmail.com>
2020-10-18 22:49:12 -0500

    create user summary class for showing the summary of the user, such as retrieving the user's information

---
+++ nexus/src/main/java/com/app/nexus/information/UserSummary.java
@@ -0,0 +1,51 @@
+package com.app.nexus.information;
+
+/**
+ * @Author Amadeus
+ */
+
+public class UserSummary {
+    private Long id;
+    private String username;
+    private String first;
+    private String last;
+
+    public UserSummary(Long id, String username, String first, String last) {
+        this.id = id;
+        this.username = username;
+        this.first = first;
+        this.last = last;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+}
