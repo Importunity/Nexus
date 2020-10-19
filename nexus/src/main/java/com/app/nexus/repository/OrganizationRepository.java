caf5b4d74fced376090f62c4a1b0e14ddd0f9045
Amadeus Her <Amadeusherr@gmail.com>
2020-10-18 22:42:06 -0500

    add author

--- nexus/src/main/java/com/app/nexus/repository/OrganizationRepository.java
+++ nexus/src/main/java/com/app/nexus/repository/OrganizationRepository.java
@@ -1,5 +1,9 @@
 package com.app.nexus.repository;
 
+/**
+ * @Author Amadeus
+ */
+
 import com.app.nexus.model.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
