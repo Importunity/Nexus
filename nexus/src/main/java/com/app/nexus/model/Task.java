37f5d8a4580d73c9ffd7557ce0f370f636559da8
Amadeus Her <Amadeusherr@gmail.com>
2020-10-18 22:41:14 -0500

    Add accessors, mutators , and extending user date audit

--- nexus/src/main/java/com/app/nexus/model/Task.java
+++ nexus/src/main/java/com/app/nexus/model/Task.java
@@ -1,9 +1,11 @@
 package com.app.nexus.model;
 
+import com.app.nexus.audit.ApplicationUserDateAudit;
 import lombok.Data;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 /**
@@ -13,7 +15,7 @@ import javax.validation.constraints.Size;
 @Entity
 @Data
 @Table(name = "tasks")
-public class Task {
+public class Task extends ApplicationUserDateAudit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
@@ -28,7 +30,7 @@ public class Task {
     @Column(name = "description")
     private String description;
 
-    @NotBlank
+    @NotNull(message = "Please enter in level")
     @Column(name = "level")
     private int level;
 
@@ -36,4 +38,43 @@ public class Task {
     @JoinColumn(name = "project_id")
     private Project project;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
 }
