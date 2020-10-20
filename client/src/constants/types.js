f8894831b1f393fada543a828022cb0eb22c183c
Amadeus Her <Amadeusherr@gmail.com>
2020-10-19 21:45:36 -0500

    Create constants folder for types

---
+++ client/src/constants/types.js
@@ -0,0 +1,11 @@
+// api
+export const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:5000/api';
+export const TOKEN = 'token';
+// user constants
+export const NAME_MIN_LENGTH = 1;
+export const NAME_MAX_LENGTH = 50;
+export const USERNAME_MIN_LENGTH = 5;
+export const USERNAME_MAX_LENGTH = 20;
+export const EMAIL_MAX_LENGTH = 40;
+export const PASSWORD_MIN_LENGTH = 8;
+export const PASSWORD_MAX_LENGTH = 50;
