eb3a1c038d0baf5657f0d34b23a813afba29babf
Amadeus Her <Amadeusherr@gmail.com>
2020-10-22 13:37:58 -0500

    Add the ability to create task whenever the user is logged in, injecting taskservice

--- nexus/src/main/java/com/app/nexus/controller/TaskController.java
+++ nexus/src/main/java/com/app/nexus/controller/TaskController.java
@@ -1,14 +1,17 @@
 package com.app.nexus.controller;
 
 import com.app.nexus.information.UserPrincipal;
+import com.app.nexus.model.ApplicationUser;
 import com.app.nexus.model.Task;
 import com.app.nexus.repository.ApplicationUserRepository;
+import com.app.nexus.repository.ProjectRepository;
 import com.app.nexus.repository.TaskRepository;
 import com.app.nexus.request.TaskRequest;
 import com.app.nexus.response.APIResponse;
 import com.app.nexus.response.TaskResponse;
 import com.app.nexus.security.CurrentUser;
 import com.app.nexus.service.TaskService;
+import org.apache.catalina.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,14 +37,21 @@ public class TaskController {
     private ApplicationUserRepository applicationUserRepository;
 
     @Autowired
+    private ProjectRepository projectRepository;
+
+    @Autowired
     private TaskService taskService;
 
     private static final Logger logger = LoggerFactory.getLogger(TaskController.class);
 
+
+
     @PostMapping
     @PreAuthorize("hasRole('USER')")
-    public ResponseEntity<?> createTask(@Valid @RequestBody TaskRequest taskRequest){
-        Task task = taskService.createTask(taskRequest);
+    public ResponseEntity<?> createTask(@Valid @RequestBody TaskRequest taskRequest, @CurrentUser UserPrincipal currentUser){
+
+
+        Task task = taskService.createTask(taskRequest, currentUser);
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -53,6 +63,9 @@ public class TaskController {
                 .created(location)
                 .body(new APIResponse(true, "Task Created"));
     }
+
+
+
 
     @GetMapping("/{taskId}")
     public TaskResponse getTaskById(@CurrentUser UserPrincipal currentUser, @PathVariable Long taskId){
