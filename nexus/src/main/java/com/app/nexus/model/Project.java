a84c2f693068ea9d546ef041b55b506d1f854e86
Amadeus Her <Amadeusherr@gmail.com>
2020-10-22 13:48:08 -0500

    Create Project model

--- nexus/src/main/java/com/app/nexus/model/Project.java
+++ nexus/src/main/java/com/app/nexus/model/Project.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 /**
@@ -22,13 +23,57 @@ public class Project extends DateAudit {
 
     @NotBlank
     @Column(name = "name")
+    @Size(max = 50)
     private String name;
 
-    @ManyToOne
-    @JoinColumn(name = "client_id")
-    private Client client;
+    @NotBlank
+    @Column(name = "description")
+    @Size(max = 100)
+    private String description;
 
     // a project can have multiple tasks
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "project")
     private Set<Task> tasks;
+
+    //@ManyToMany(fetch = FetchType.LAZY, mappedBy = "projects")
+    //private Set<ApplicationUser> applicationUsers;
+
+    public Project(){}
+
+    public Project(Long id, @NotBlank String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    /*public Set<ApplicationUser> getApplicationUsers() {
+        return applicationUsers;
+    }
+
+    public void setApplicationUsers(Set<ApplicationUser> applicationUsers) {
+        this.applicationUsers = applicationUsers;
+    }*/
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
