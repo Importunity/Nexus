cc591f79682ad0cc2f5b2aff0010b2e22574663d
Amadeus Her <Amadeusherr@gmail.com>
2020-10-22 13:36:53 -0500

    Add more constants for error checking in registration form

--- client/src/constants/types.js
+++ client/src/constants/types.js
@@ -2,10 +2,13 @@
 export const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:5000/api';
 export const TOKEN = 'token';
 // user constants
-export const NAME_MIN_LENGTH = 1;
-export const NAME_MAX_LENGTH = 50;
+export const FIRST_NAME_MIN_LENGTH = 1;
+export const FIRST_NAME_MAX_LENGTH = 50;
+export const LAST_NAME_MIN_LENGTH = 1;
+export const LAST_NAME_MAX_LENGTH = 50;
 export const USERNAME_MIN_LENGTH = 5;
-export const USERNAME_MAX_LENGTH = 20;
+export const USERNAME_MAX_LENGTH = 40;
 export const EMAIL_MAX_LENGTH = 40;
 export const PASSWORD_MIN_LENGTH = 8;
-export const PASSWORD_MAX_LENGTH = 50;
+export const PASSWORD_MAX_LENGTH = 20;
+export const PHONE_MAX_LENGTH = 20;
