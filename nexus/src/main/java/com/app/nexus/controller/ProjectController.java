0227bc35aabac5b14982bdce5eae6b86baf3e0f3
Amadeus Her <Amadeusherr@gmail.com>
2020-10-22 13:54:08 -0500

    Create project and inject with application user and project service

---
+++ nexus/src/main/java/com/app/nexus/controller/ProjectController.java
@@ -0,0 +1,60 @@
+package com.app.nexus.controller;
+
+import com.app.nexus.information.UserPrincipal;
+import com.app.nexus.model.Project;
+import com.app.nexus.repository.ApplicationUserRepository;
+import com.app.nexus.repository.ProjectRepository;
+import com.app.nexus.request.ProjectRequest;
+import com.app.nexus.response.APIResponse;
+import com.app.nexus.security.CurrentUser;
+import com.app.nexus.service.ProjectService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+/**
+ * @Author Amadeus
+ */
+
+@RestController
+@RequestMapping("/api/projects")
+public class ProjectController {
+    @Autowired
+    private ApplicationUserRepository applicationUserRepository;
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    private ProjectService projectService;
+
+    private static final Logger logger = LoggerFactory.getLogger(ProjectController.class);
+
+    @PostMapping
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<?> createProject(@Valid @RequestBody ProjectRequest projectRequest, @CurrentUser UserPrincipal currentUser){
+
+
+        Project project = projectService.createProject(projectRequest, currentUser);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{projectId}")
+                .buildAndExpand(project.getId())
+                .toUri();
+
+        return ResponseEntity
+                .created(location)
+                .body(new APIResponse(true, "Project Created"));
+    }
+}
