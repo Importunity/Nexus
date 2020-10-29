c2c9d0dab8909a1355af1cfe733f8f4679da8d4e
Amadeus Her <Amadeusherr@gmail.com>
2020-10-28 20:31:29 -0500

    Create get task request

--- client/src/api/TaskAPI.js
+++ client/src/api/TaskAPI.js
@@ -1,15 +1,5 @@
 import { BASE_URL } from "../constants/types";
 import { request } from "./RequestJSON";
 
-export function getAllTasks(){
-    return request(
-        
-    )
-}
 
-export function getUserTasks(username){
-    return request({
-        url: BASE_URL + "/users/" + username,
-        method: 'GET'
-    })
-}
+
