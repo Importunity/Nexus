437c583ea47ff79b31198ebb0278aff0a1ad5c69
Amadeus Her <Amadeusherr@gmail.com>
2020-10-29 15:41:09 -0500

    Add the ability to find by name to project repository

--- nexus/src/main/java/com/app/nexus/repository/ProjectRepository.java
+++ nexus/src/main/java/com/app/nexus/repository/ProjectRepository.java
@@ -6,8 +6,14 @@ package com.app.nexus.repository;
 
 import com.app.nexus.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    Optional<Project> findByName(String name);
 }
