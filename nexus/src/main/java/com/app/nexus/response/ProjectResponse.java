68e3ba658a6edb42d205e16598005a10898ef94b
Amadeus Her <Amadeusherr@gmail.com>
2020-10-29 15:48:16 -0500

    Create project response

---
+++ nexus/src/main/java/com/app/nexus/response/ProjectResponse.java
@@ -0,0 +1,54 @@
+package com.app.nexus.response;
+
+import com.app.nexus.model.Project;
+import com.app.nexus.model.Task;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @Author Amadeus
+ */
+
+public class ProjectResponse {
+    private Long id;
+    private String name;
+    private String description;
+    @JsonIgnoreProperties
+    private Set<Task> tasks;
+
+
+    public Set<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Set<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
