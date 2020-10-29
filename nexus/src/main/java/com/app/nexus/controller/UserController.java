deb967b007a04358baf8193fc5cccc6f983063b9
Amadeus Her <Amadeusherr@gmail.com>
2020-10-29 15:35:02 -0500

    Add get current user projects, however this edit will be removed later

--- nexus/src/main/java/com/app/nexus/controller/UserController.java
+++ nexus/src/main/java/com/app/nexus/controller/UserController.java
@@ -3,10 +3,15 @@ package com.app.nexus.controller;
 import com.app.nexus.information.UserIdentityAvailability;
 import com.app.nexus.information.UserPrincipal;
 import com.app.nexus.information.UserSummary;
+import com.app.nexus.model.Project;
 import com.app.nexus.repository.ApplicationUserRepository;
 import com.app.nexus.repository.TaskRepository;
+import com.app.nexus.request.ProjectRequest;
+import com.app.nexus.response.ProjectResponse;
 import com.app.nexus.response.TaskResponse;
 import com.app.nexus.security.CurrentUser;
+import com.app.nexus.service.CustomUserDetailsService;
+import com.app.nexus.service.ProjectService;
 import com.app.nexus.service.TaskService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +20,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * @Author Amadeus
@@ -29,13 +35,27 @@ public class UserController {
     private TaskRepository taskRepository;
 
     @Autowired
+    CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
     private TaskService taskService;
+
+    @Autowired
+    private ProjectService projectService;
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping("/user/me")
     @PreAuthorize("hasRole('USER')")
     public UserSummary getCurrentUser(@CurrentUser UserPrincipal currentUser){
+        //System.out.println(currentUser.getFirst());
+        UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getFirst(), currentUser.getLast());
+        return userSummary;
+    }
+
+    @GetMapping("/user/project")
+    @PreAuthorize("hasRole('USER')")
+    public UserSummary getCurrentUserProjects(@CurrentUser UserPrincipal currentUser){
         UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getFirst(), currentUser.getLast());
         return userSummary;
     }
@@ -56,6 +76,7 @@ public class UserController {
     public List<TaskResponse> getTasksCreatedBy(@PathVariable(value = "username") String username, @CurrentUser UserPrincipal currentUser){
         return taskService.getTasksCreatedBy(username, currentUser);
     }
+
 
 
 }
