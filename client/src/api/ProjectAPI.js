e15217b35ec893ce921913da1d3fc94901787f32
Amadeus Her <Amadeusherr@gmail.com>
2020-10-29 15:46:39 -0500

    Create getprojects api and create projects api

---
+++ client/src/api/ProjectAPI.js
@@ -0,0 +1,23 @@
+import { BASE_URL } from '../constants/types';
+import {request} from './RequestJSON';
+
+export function createProject(projectRequest){
+    return request({
+        url: BASE_URL + "/projects",
+        method: "POST",
+        body: JSON.stringify(projectRequest)
+    })
+}
+
+
+export function userloadProjects(projectRequest){
+    return request({
+        url: BASE_URL + "/projects",
+        method: "GET",
+        body: JSON.stringify(projectRequest)
+    })
+
+}
+
+
+
