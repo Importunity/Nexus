3ccbc35bc4acc8fe39a7bb128e18758e8d645d68
Amadeus Her <Amadeusherr@gmail.com>
2020-11-10 14:18:38 -0600

    Add refresh token to authentication response

--- nexus/src/main/java/com/app/nexus/payload/response/JWTAuthenticationResponse.java
+++ nexus/src/main/java/com/app/nexus/payload/response/JWTAuthenticationResponse.java
@@ -1,4 +1,4 @@
-package com.app.nexus.response;
+package com.app.nexus.payload.response;
 
 /**
  * @Author Amadeus
@@ -9,7 +9,31 @@ package com.app.nexus.response;
 
 public class JWTAuthenticationResponse {
     private String token;
+    private String refreshToken;
     private String bearer = "Bearer";
+    private Long expires;
+
+    public JWTAuthenticationResponse(String token, String refreshToken, Long expires) {
+        this.token = token;
+        this.refreshToken = refreshToken;
+        this.expires = expires;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public Long getExpires() {
+        return expires;
+    }
+
+    public void setExpires(Long expires) {
+        this.expires = expires;
+    }
 
     public JWTAuthenticationResponse(String token){
         this.token = token;
